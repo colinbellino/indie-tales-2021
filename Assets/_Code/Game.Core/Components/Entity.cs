fcb02990dea4c8b687c0f528f3cd2a000e7fab57
Colin Bellino <bellino.colin@gmail.com>
2021-07-11 16:03:30 +0200

    Backup from Unity Collab:
    - Add player sprites and animation, tree & rocks platforms
    - trees and skies!
    - added a walking main character!
    - stoney platforms!
    - Restart level when player hits a killbox
    - Add project settings
    - Water all the things
    - a nice building
    - Fix ropes, again
    - Recommit these files since Unity Collab rollbacked them without asking... Thanks!

--- Assets/_Code/Game.Core/Components/Entity.cs
+++ Assets/_Code/Game.Core/Components/Entity.cs
@@ -4,24 +4,41 @@ using UnityEngine.UI;
 
 namespace Game.Core
 {
-    public class Entity : MonoBehaviour
-    {
-        [SerializeField] public Rigidbody2D Rigidbody;
-        [SerializeField] public CharacterController2D Controller;
-        [SerializeField] public Animator Animator;
-        [SerializeField] public AudioSource AudioSource;
 
-        [SerializeField] public float Gravity = -25f;
-        [SerializeField] public float RunSpeed = 8f;
-        [SerializeField] public float GroundDamping = 20f;
-        [SerializeField] public float InAirDamping = 5f;
-        [SerializeField] public float JumpHeight = 3f;
+	public class Entity : MonoBehaviour
+	{
+		[SerializeField] public Rigidbody2D Rigidbody;
+		[SerializeField] public CharacterController2D Controller;
+		[SerializeField] public Animator Animator;
+		[SerializeField] public AudioSource AudioSource;
 
-        [HideInInspector] public RaycastHit2D LastControllerColliderHit;
-        [HideInInspector] public Vector3 Velocity;
-        [HideInInspector] public float NormalizedHorizontalSpeed = 0;
-        [HideInInspector] public float DigAnimationEndTimestamp;
-        [HideInInspector] public float StartDiggingTimestamp;
-        [HideInInspector] public Vector3Int DigDirection;
-    }
+		[SerializeField] public float Gravity = -25f;
+		[SerializeField] public float MoveSpeed = 8f;
+		[SerializeField] public float GroundDamping = 20f;
+		[SerializeField] public float InAirDamping = 5f;
+		[SerializeField] public float JumpHeight = 3f;
+		[SerializeField] public float DetectionRadius = 3f;
+		[SerializeField] public float FleeDelay = 0.5f;
+
+		[SerializeField] public bool ControlledByAI;
+
+		[HideInInspector] public RaycastHit2D LastControllerColliderHit;
+		[HideInInspector] public Vector3 Velocity;
+		[HideInInspector] public float NormalizedHorizontalSpeed = 0;
+		[HideInInspector] public float AnimationTimestamp;
+		[HideInInspector] public Vector3 MoveDestination;
+		[HideInInspector] public float SpawnTimestamp;
+		[HideInInspector] public float FleeTimestamp;
+		[HideInInspector] public float StartMoveTimestamp;
+		[HideInInspector] public bool Moving;
+		[HideInInspector] public AIStates AIState;
+	}
+
+	public enum AIStates
+	{
+		WaitingToSpawn,
+		MovingToDestination,
+		Idle,
+		Fleeing,
+	}
 }
